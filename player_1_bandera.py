b12ecc0f55d8ea7622aefa43c5374eabbc4d4765
Joel Gómez Santos <123945089+joegom22@users.noreply.github.com>
2023-05-13 21:51:10 +0200

    Comentarios finales
    Comentarios que faltaban a las funciones

--- player_1_bandera.py
+++ player_1_bandera.py
@@ -95,26 +95,70 @@ class Flag():
 
 class Wall():
     def __init__(self):
+        """
+        Función de la clase Wall que inicializa un muro con posición y medidas vacías
+        
+        args
+            self
+        """
         self.pos = [None, None]
         self.measures = [None, None]
 
     def get_pos(self):
+        """
+        Función de la clase Wall que devuelve la posición del muro
+        
+        args
+            self
+        """
         return self.pos
     
     def get_measures(self):
+        """
+        Función de la clase Wall que devuelve las medidas del muro
+        
+        args
+            self
+        """
         return self.measures
 
     def set_pos(self, pos):
+        """
+        Función de la clase Wall que cambia la posición del muro por una dada
+        
+        args
+            self
+            pos: [int, int] --> nueva posición
+        """
         self.pos = pos
         
     def set_measures(self, meas):
+        """
+        Función de la clase Wall que cambia las medidas del muro por unas dadas
+        
+        args
+            self
+            meas: [int, int] --> nuevas medidas
+        """
         self.measures=meas    
 
     def __str__(self):
+        """
+        Función de la clase Wall que permite visualizar por pantalla el estado del muro
+        
+        args
+            self
+        """
         return f"W<{self.pos}>"
     
 class Game():
     def __init__(self):
+        """
+        Función de la clase Game que inicializa un juego con sus jugadores, banderas, bandera visible, muros, puntuación y valor de jugando o no todos vacíos
+        
+        args
+            self
+        """
         self.players = [Player(0), Player(1)]
         self.flags = [ Flag() for i in range(len(st.banderas)) ]
         self.flagsvisible = [Flag()]
@@ -123,12 +167,34 @@ class Game():
         self.running = True
         
     def get_player(self, n_player):
+        """
+        Función de la clase Game que dado un número de jugador devuelve el jugador con dicho número
+        
+        args
+            self
+            n_player: int --> número de jugador
+        """
         return self.players[n_player]
 
     def set_pos_player(self, n_player, pos):
+        """
+        Función de la clase Game que dado un número de jugador y una posición cambia la posición del jugador con dicho número a la posición dada
+                
+        args
+            self
+            n_player: int --> número de jugador
+            pos: [int, int] --> nueva posición
+        """
         self.players[n_player].set_pos(pos)
         
     def set_walls(self, info):
+        """
+        Función de la clase Game que dada la información sobre unos muros establece como muros del juego dichos muros
+                
+        args
+            self
+            info: [[[int, int], [int, int]]] --> posiciones y medidas
+        """
         j=0
         for i in info:
             self.walls[j].set_pos(i[0])
@@ -136,18 +202,51 @@ class Game():
             j+=1
             
     def get_flag(self):
+        """
+        Función de la clase Game que devuelve la primera bandera visible
+                
+        args
+            self
+        """
         return self.flagsvisible[0]
     
     def set_flag_pos(self, pos):
+        """
+        Función de la clase Game que dada la información sobre unas banderas establece como banderas del juego dichas banderas
+                
+        args
+            self
+            info: [[int, int]] --> posiciones
+        """
         self.flagsvisible[0].set_pos(pos)
 
     def get_score(self):
+        """
+        Función de la clase Game que devuelve la puntuación
+        
+        args
+            self
+        """
         return self.score
 
     def set_score(self, score):
+        """
+        Función de la clase Game que cambia la puntuación por una dada
+        
+        args
+            self
+            score: [int, int] --> nueva puntuación
+        """
         self.score = score
         
     def update(self, gameinfo):
+        """
+        Función de la clase Game que llama a las funciones apropiadas para actualizar toda la información del juego por una nueva dada
+        
+        args
+            self
+            gameinfo: Dict --> Nueva información
+        """
         self.set_pos_player(0, gameinfo['pos_player_0'])
         self.set_pos_player(1, gameinfo['pos_player_1'])
         self.set_walls(gameinfo['walls'])
@@ -156,16 +255,41 @@ class Game():
         self.running = gameinfo['is_running']
 
     def is_running(self):
+        """
+        Función de la clase Game que devuelve si el juego está corriendo o no
+        
+        args
+            self
+        """
         return self.running
 
     def stop(self):
+        """
+        Función de la clase Game que cambia el valor de si le juego está corriendo a falso
+        
+        args
+            self
+        """
         self.running = False
 
     def __str__(self):
+        """
+        Función de la clase Game que nos permite visualizar por pantalla el estado de los dos jugadores del juego
+        
+        args
+            self
+        """
         return f"G<{self.players[0]}:{self.players[1]}>"
     
 class PlayerSprite(pg.sprite.Sprite):
     def __init__(self, player):
+        """
+        Función de la clase PlayerSprite que dado un jugador inicializa el sprite del jugador en su posición a una imagen concreta en función de su número
+        
+        args
+            self
+            player: Player --> Jugador
+        """
         self.player=player
         self.n_player=player.n_player
         super().__init__()
@@ -181,15 +305,35 @@ class PlayerSprite(pg.sprite.Sprite):
         self.vel = [0,0]
         self.acc = [0,0]
         self.score = 0
+        
     def update(self):
+        """
+        Función de la clase PlayerSprite que actualiza la posición del sprite a la posición del jugador
+        
+        args
+            self
+        """
         pos = self.player.get_pos()
         self.rect.centerx, self.rect.centery = pos
 
     def __str__(self):
+        """
+        Función de la clase PlayerSprite que permite visualizar por pantalla el estado del jugador del sprite
+        
+        args
+            self
+        """
         return f"S<{self.player}>"   
 
 class FlagSprite(pg.sprite.Sprite):
     def __init__(self, flag):
+        """
+        Función de la clase FlagSprite que dada una bandera inicializa el sprite de la bandera en su posición a una imagen concreta
+        
+        args
+            self
+            flag: Flag --> Bandera
+        """
         super().__init__()
         self.flag=flag
         self.image = pg.image.load(st.bandera_roja).convert()
@@ -202,11 +346,24 @@ class FlagSprite(pg.sprite.Sprite):
         self.image.set_colorkey(st.BLACK) #esto sirve para que se defina la silueta bien
         
     def update(self):
+        """
+        Función de la clase FlagSprite que cambia la posición del sprite por la posición de la bandera asociada
+        
+        args
+            self
+        """
         pos = self.flag.get_pos()
         self.rect.centerx, self.rect.centery = pos
     
 class WallSprite(pg.sprite.Sprite):
     def __init__(self, wall):
+        """
+        Función de la clase WallSprite que dado un muro inicializa el sprite del muro en su posición a un rectángulo de iguales dimensiones que las del muro
+        
+        args
+            self
+            wall: Wall --> Muro
+        """
         self.wall = wall
         super().__init__()
         self.image = pg.Surface((self.wall.get_measures())) #crea un rectangulo de grosor w y altura h
@@ -220,6 +377,13 @@ class WallSprite(pg.sprite.Sprite):
         
 class Display():
     def __init__(self, game):
+        """
+        Función de la clase Display que dado un juego incializa lo que se muestra por pantalla con la información que recibe del juego
+        
+        args
+            self
+            game: Game --> juego del que recibe la información
+        """
         pg.init()
         self.screen = pg.display.set_mode(st.SIZE)
         self.clock =  pg.time.Clock()  #FPS
@@ -247,6 +411,13 @@ class Display():
         pg.display.flip()
     
     def analyze_events(self, n_player):
+        """
+        Función de la clase Display que dado un número de jugador analiza y devuelve los eventos que ocurren para ese jugador
+        
+        args
+            self
+            n_player: int --> número de jugador
+        """
         events = []
         for event in pg.event.get():
             if event.type == pg.KEYDOWN:
@@ -271,7 +442,13 @@ class Display():
         return events  
 
     def refresh(self):
-    	flag = self.game.get_flag()
+    	"""
+        Función de la clase Display que vuelve a cargar la pantalla de visualización
+        
+        args
+            self
+        """
+        flag = self.game.get_flag()
     	self.flag = FlagSprite(flag)
     	self.all_sprites.add(self.flag)
     	self.flag_sprites = pg.sprite.Group()
@@ -289,7 +466,13 @@ class Display():
     	pg.display.flip()
         
     def display_end(self):
-    	score = self.game.get_score()
+    	"""
+        Función de la clase Display que muestra la pantalla de final de juego acorde al jugador ganador
+        
+        args
+            self
+        """
+        score = self.game.get_score()
     	if score[0] == 3:
     	    self.screen = pg.display.set_mode([1000,650])
     	    self.background = pg.image.load(st.fin_p0).convert()
@@ -302,13 +485,26 @@ class Display():
     	    pg.display.flip()
             
     def tick(self):
+        """
+        Función de la clase Display para actualizar el reloj
+        
+        args
+            self
+        """
         self.clock.tick(st.FPS)
 
     @staticmethod
     def quit():
+        """
+        Función sin argumentos de la clase Display que sale del juego
+        """
         pg.quit()
         
 def main():
+    """
+    Función principal que se ejecuta al ejecutar el programa.
+    Se conecta a la sala y recibe la información del juego, va enviando los eventos a la sala para que esta los ejecute y va actualizando la información con lo que la sala le manda
+    """
     try:
         with Client(("10.9.91.199", 5000), authkey=b'secret password') as conn:
             game = Game()

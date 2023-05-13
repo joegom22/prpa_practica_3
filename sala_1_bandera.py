1dc71163247f2852727ecef3890894af3e713582
Joel Gómez Santos <123945089+joegom22@users.noreply.github.com>
2023-05-13 19:05:37 +0200

    Update sala_1_bandera.py

--- sala_1_bandera.py
+++ sala_1_bandera.py
@@ -55,7 +55,7 @@ class Player():
         
         args
             self
-            pos: (int, int) --> nueva posición
+            pos: [int, int] --> nueva posición
         """        
         self.pos = pos    
     
